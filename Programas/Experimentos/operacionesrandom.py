d4fe00e2b3690f760100a40772039291a11e9171
Franco Benassi <fbenassi2019@alu.uct.cl>
2020-11-14 00:41:26 -0300

    cambios v2

--- Programas/Experimentos/operacionesrandom.py
+++ Programas/Experimentos/operacionesrandom.py
@@ -1,7 +1,18 @@
-def saludo():
-    print("hola como estás?")
-
-saludo()
-
-
-print("Hola que tal")
+class Coche:
+    """Abstraccion de los objetos coche."""
+    def __init__(self, gasolina):
+        self.gasolina = gasolina
+        print("Hay", gasolina, "litros")
+        
+    def arrancar(self, valor):
+        if self.gasolina > 0:
+            print("Arranca")
+        else:
+            print("No Arranca")
+                    
+    def conducir(self):
+        if self.gasolina > 0:
+            self.gasolina -= 1
+            print("Quedan", self.gasolina, "litros")
+        else:
+            print("No se mueve")
