d129f6c96efeb1476db25fe72fbabae0997a55f4
Franco Benassi <fbenassi2019@alu.uct.cl>
2021-08-01 15:37:57 -0400

    ligeros

--- Programas/fichero Python - Flask/Flask/primerFlask.py
+++ Programas/fichero Python - Flask/Flask/primerFlask.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 def Principal():
     return "<h1> Texto realizado con flask </h1>"
 
+@app.route("/adios")
+def despedirse():
+    return "<h3>Hasta Luego, nos vemos</h3>"
+
+# Ruta Dinámica
+@app.route("/saludar/<nombre>")
+def saludar(nombre):
+    return "<h1>Hola</h1>"+ nombre + "<h1>Buenas Tardes!!</h1>"
+
 if __name__ == '__main__':
     app.run()
 
